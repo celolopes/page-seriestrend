79617070ea6efd6244b4f95fe2e9bf925b6c3889
Marcelo Lopes <marcelo.cae@gmail.com>
2026-01-05 15:03:05 -0300

    feat: Configure Next.js image remote patterns to allow images from image.tmdb.org.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
