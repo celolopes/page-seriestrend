6ae114013bf0838e89322dd71a825bac4a118288
Marcelo Lopes <marcelo.cae@gmail.com>
2026-01-05 11:27:44 -0300

    chore: Remove `eslint.ignoreDuringBuilds` configuration from Next.js.

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
-  eslint: {
-    // Avisa sobre problemas ESLint, mas não impede o build de completar
-    ignoreDuringBuilds: true,
-  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
